ba6fddb627bd97d606a11502b6702da78c3a9f46
ShaD971 <kevin.toleon@hotmail.fr>
2016-04-09 15:14:20 +0200

    1er commit

---
+++ acceuil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tests PHP </title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <?php include("entete.php");?>
+         <?php include("menus.php");?>
+
+            <div id="corps">
+        <h1>Mon  site en PHP</h1>
+        
+        <p>
+            Bienvenue sur mon super site !<br />
+            Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
+        </p>
+    </div>
+    
+    <?php include("pied_de_page.php"); ?>
+
+    </body>
+</html>
