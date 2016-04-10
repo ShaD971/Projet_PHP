92f58a5a6fb49e86c3755e597ba6dbc5b5f1d5fe
ShaD971 <kevin.toleon@hotmail.fr>
2016-04-10 23:36:46 +0200

    modif entete et menu

--- acceuil.php
+++ acceuil.php
@@ -7,6 +7,7 @@
     </head>
     <body>
         <?php include("entete.php");?>
+         <?php include("menu.php");?>
 
          
     
--- menu.php
+++ menu.php
@@ -1,12 +1,12 @@
     <!-- Le menu -->
     
-    <nav id="menu">        
-        <div class="navigation">
+            
+        <nav class="menu">
             <h3>Menu</h3>
             <ul>
                 <li><a href="acceuil.php">Acceuil</a></li>
                 <li><a href="page2.html">Lien</a></li>
                 <li><a href="page3.html">Lien</a></li>
             </ul>
-        </div>    
-    </nav>
+        </nav>    
+   
