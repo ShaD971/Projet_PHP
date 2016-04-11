8aa8a9db66d72f33e51937c8bebf3f97bbe1108a
ShaD971 <kevin.toleon@hotmail.fr>
2016-04-11 21:12:03 +0200

    corection

--- menu.php
+++ menu.php
@@ -5,8 +5,8 @@
             <h3>Menu</h3>
             <ul>
                 <li><a href="acceuil.php">Acceuil</a></li>
-                <li><a href="page2.html">Lien</a></li>
-                <li><a href="page3.html">Lien</a></li>
+                <li><a href="page2.php">Lien</a></li>
+                <li><a href="page3.php">Lien</a></li>
             </ul>
         </nav>    
    
--- tchat-Post.php
+++ tchat-Post.php
@@ -19,11 +19,11 @@
                 die('Erreur : '.$e->getMessage());
             }   
 
-            // Insertion du message à l'aide d'une requête préparée
+            // Insertion du message à l'aide d'une requête 
             $req = $bdd->prepare('INSERT INTO tchat (pseudo, message) VALUES(?, ?)');
             $req->execute(array($_POST['pseudo'], $_POST['message']));
 
-            // Redirection du visiteur vers la page du minichat
+            // Redirection du visiteur vers la page du tchat
             header('Location: acceuil.php');
         ?>
 
